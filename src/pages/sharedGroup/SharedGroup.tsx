407fc4964115a5a86993bef408f2bd873b559fff
bipin-work <bipinshrestha.work@gmail.com>
2024-06-12 22:51:59 +0545

    Add page container to shared group

--- src/pages/sharedGroup/SharedGroup.tsx
+++ src/pages/sharedGroup/SharedGroup.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import PageContainer from "../../components/PageContainer/PageContainer";
 
 const SharedGroup = () => {
-  return <>SharedGroup</>;
+  return (
+    <PageContainer>
+      <Header headerName="Group" />
+    </PageContainer>
+  );
 };
 export default SharedGroup;
