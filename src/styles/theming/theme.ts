255a65238e714aa5d2592f61278712385eef2f11
bipin-work <bipinshrestha.work@gmail.com>
2024-06-12 23:46:22 +0545

    Add theme configs for button and checkbox

--- src/styles/theming/theme.ts
+++ src/styles/theming/theme.ts
@@ -5,6 +5,35 @@ const config = {
   useSystemColorMode: false,
 };
 
+const components = {
+  Button: {
+    variants: {
+      solid: {
+        bg: "purple.500", // Default background color for solid variant
+        color: "white", // Default text color for solid variant
+        _hover: {
+          bg: "purple.600", // Background color on hover for solid variant
+        },
+      },
+    },
+  },
+  Checkbox: {
+    baseStyle: {
+      control: {
+        _checked: {
+          bg: "purple.500", // background color when checked
+          borderColor: "pruple.500", // border color when checked
+          color: "white", // checkmark color
+          _hover: {
+            bg: "purple.600", // background color when checked and hovered
+            borderColor: "pruple.600", // border color when checked and hovered
+          },
+        },
+      },
+    },
+  },
+};
+
 const colors = {
   brand: {
     bg: "#8687E7",
@@ -23,7 +52,7 @@ const fonts = {
   body: "'Inter', sans-serif",
 };
 
-const theme = extendTheme({ config, breakpoints, fonts, colors });
+const theme = extendTheme({ config, breakpoints, fonts, colors, components });
 export const bgTheme = { light: "gray.100", dark: "gray.700" };
 
 export default theme;
